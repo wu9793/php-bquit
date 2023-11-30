c8d44c0b832d4855481246840d1bbb3c20e8141e
wu <timmy61501@gmail.com>
2023-11-30 16:23:57 +0800

    完成刪除、編輯、顯示功能

--- q1/edit_title.php
+++ q1/edit_title.php
@@ -3,28 +3,17 @@
 include_once "db.php";
 dd($_POST);
 
-foreach($_POST['id'] as $key => $id){
-    $row=$Title->find($id);
-    // dd($row);
-    $row['text']=$_POST['text'][$key];
-    $Title->save($row);
+foreach ($_POST['id'] as $key => $id) {
+
+    if (isset($_POST['del']) && in_array($id, $_POST['del'])) {
+        $Title->del($id);
+    } else {
+        $row = $Title->find($id);
+        $row['text'] = $_POST['text'][$key];
+        $row['view'] = ($id == $_POST['view']) ? 1 : 0;
+        $Title->save($row);
+
+    }
 }
 
-foreach($_POST['id'] as $id){
-
-    $row=$Title->find($id);
-    // if($id==$_POST['view']){
-    //     $row['view']=1;
-    // }else{
-    //     $row['view']=0;
-    // }
-    $row['view']=($id==$_POST['view'])?1:0;
-    $Title->save($row);
-}
-
-// $_POST['view']=0;
-
-// $Title->save($_POST);
-
-// header("location:index.php");
-?>
+header("location:index.php");

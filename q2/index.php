791822fa4b6d5a1a991cbf76d5a1e0c2081f344e
wu <timmy61501@gmail.com>
2023-12-01 09:35:49 +0800

    建立問卷調查前端列表頁面版型

---
+++ q2/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>問卷調查</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+</head>
+
+<body>
+    <header class="p-5">
+        <h1 class="text-center">問卷調查</h1>
+    </header>
+    <main class="container">
+    <fieldset class="form-group border p-3">
+        <legend>目前位置:首頁 > 問卷調查</legend>
+        <table class="table">
+            <tr>
+                <th>編號</th>
+                <th>問卷題目</th>
+                <th>投票總數</th>
+                <th>結果</th>
+                <th>狀態</th>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+    </fieldset>
+
+
+        <script src="../js/jquery-3.4.1.min.js"></script>
+        <script src="../js/bootstrap.js"></script>
+    </main>
+</body>
+
+</html>
